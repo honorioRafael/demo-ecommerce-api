8d19e1ba72c18e2b391c5aa70957d0aa92765f6f
Rafael Honorio <133076581+honorioRafael@users.noreply.github.com>
2026-05-05 11:43:23 -0300

    refactor(Address): Modificado get para trazer apenas itens do usuario logado

--- src/ECommerce.Infrastructure/Persistence/Repositories/AddressRepository.cs
+++ src/ECommerce.Infrastructure/Persistence/Repositories/AddressRepository.cs
@@ -1,11 +1,31 @@
+using ECommerce.Application.Interfaces.Security;
 using ECommerce.Domain.Entities;
 using ECommerce.Domain.Interfaces.Repositories;
 using ECommerce.Infrastructure.Persistence.Contexts;
 using ECommerce.Infrastructure.Persistence.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Infrastructure.Persistence.Repositories;
 
 public class AddressRepository : BaseRepository<Address>, IAddressRepository
 {
-    public AddressRepository(AppDbContext context) : base(context) { }
+    private readonly ICurrentUserService _currentUserService;
+
+    public AddressRepository(AppDbContext context, ICurrentUserService currentUserService) : base(context)
+    {
+        _currentUserService = currentUserService;
+    }
+
+    public override async Task<List<Address>> GetAllAsync(int pageIndex, int pageSize, bool asNoTracking = true, CancellationToken cancellationToken = default)
+    {
+        if (pageIndex < 0) throw new ArgumentOutOfRangeException(nameof(pageIndex));
+        if (pageSize <= 0) throw new ArgumentOutOfRangeException(nameof(pageSize));
+
+        var dbSet = asNoTracking ? _dbSet.AsNoTracking() : _dbSet;
+        return await dbSet
+            .Where(x => x.CreatedBy == _currentUserService.UserId)
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+    }
 }
